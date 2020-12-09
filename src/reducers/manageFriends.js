5eff3259b34773ef963896cb48875977ff4b7352
blumenthaler <blumenthaleralex@gmail.com>
2020-12-09 09:54:50 -0500

    commit tree 688bc5dd0677362dadf26b38b0512736f023809a

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,30 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            // state.friends.push(action.friend);
+            return (
+                {...state,
+                    friends: [
+                        ...state.friends,
+                        action.friend
+                    ]
+                }
+            ) 
+            //{friends: state.friends}
+        case "REMOVE_FRIEND":
+            let seeYa = state.friends.findIndex(friend => friend.id === action.id);
+            return (
+                {...state,
+                    friends: [
+                      ...state.friends.slice(0, seeYa),
+                      ...state.friends.slice(seeYa + 1)
+                    ]
+                }
+              )
+        default:
+            return state;
+    }
 }
+
+
+
